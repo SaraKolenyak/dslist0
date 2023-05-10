6246dba499e347850320a8a39b5d7d0327e30ec7
SaraKolenyak <sarakfs25@gmail.com>
2023-05-10 18:45:01 -0300

    Game seed

--- src/main/java/com/sarak/dslist/entities/Game.java
+++ src/main/java/com/sarak/dslist/entities/Game.java
@@ -16,20 +16,24 @@ public class Game {
     private Integer year;
     private String genre;
     private String platforms;
+    private Double score;
     private String imgUrl;
+    @Column (columnDefinition = "TEXT")
     private String shortDescription;
+    @Column (columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){
 
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platforms, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
         this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -73,6 +77,14 @@ public class Game {
 
     public void setPlatforms(String platforms) {
         this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
