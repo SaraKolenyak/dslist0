f4cb124078b1dc36f07c05a151513096ad1f5e10
SaraKolenyak <sarakfs25@gmail.com>
2023-05-10 17:28:10 -0300

    Projeto Criado

---
+++ src/test/java/com/sarak/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarak.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
